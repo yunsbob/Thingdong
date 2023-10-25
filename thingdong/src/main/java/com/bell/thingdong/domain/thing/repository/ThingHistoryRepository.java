3a65c327967c3cceaab3897827c66b72beed2751
윤민재 <w124564@naver.com>
2023-10-25 16:19:08 +0900

    [S09P31B304-000] feat: ThingHistory 설계

--- thingdong/src/main/java/com/bell/thingdong/domain/thing/repository/ThingHistoryRepository.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/thing/repository/ThingHistoryRepository.java
@@ -2,7 +2,7 @@ package com.bell.thingdong.domain.thing.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.bell.thingdong.domain.room.entity.UserRoom;
+import com.bell.thingdong.domain.thing.entity.ThingHistory;
 
-public interface ThingHistoryRepository extends JpaRepository<UserRoom, Long>, CustomThingHistoryRepository {
+public interface ThingHistoryRepository extends JpaRepository<ThingHistory, Long>, CustomThingHistoryRepository {
 }
