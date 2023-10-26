8f5dcdd0cdef9b06672d1f1b0890104c61e5c485
“배민경” <hellobaemin@gmail.com>
2023-10-26 17:51:25 +0900

    [S09P31B304-275] config: jotai, react-query, styled components 설치

--- frontend-web/src/index.tsx
+++ frontend-web/src/index.tsx
@@ -1,7 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import { Provider } from 'jotai';
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  rootElement,
+);
