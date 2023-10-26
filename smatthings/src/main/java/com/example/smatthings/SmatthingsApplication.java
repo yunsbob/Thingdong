b786b7a01be643d756dccecbc42a1dccadc12005
minseo0421 <k4496k@naver.com>
2023-10-26 13:03:58 +0900

    [S09P31B304-166] feat : 프로젝트 생성

---
+++ smatthings/src/main/java/com/example/smatthings/SmatthingsApplication.java
@@ -0,0 +1,14 @@
+package com.example.smatthings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmatthingsApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SmatthingsApplication.class, args);
+	}
+
+}
---
+++ smatthings/src/test/java/com/example/smatthings/SmatthingsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smatthings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmatthingsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
