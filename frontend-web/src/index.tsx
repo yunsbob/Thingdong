a4171a1ee133f80eb7342017b62c6a3b40fc309a
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-10-27 15:16:19 +0900

    [S09P31B304-278] feat: 전역  font style 설정

--- frontend-web/src/index.tsx
+++ frontend-web/src/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'jotai';
+import "frontend-web/public/font.css";
 
 const rootElement = document.getElementById('root');
-// 안녕하세요
 ReactDOM.render(
   <React.StrictMode>
     <Provider>
