5e9e1bee94b91190854f7d4acaead9768f7042b1
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-10-27 15:50:15 +0900

    [S09P31B304-208] test : globalstyle 설정 테스트

--- frontend-web/src/index.tsx
+++ frontend-web/src/index.tsx
@@ -2,14 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'jotai';
+import GlobalStyle from '@/styles/GlobalStyle';
 
 const rootElement = document.getElementById('root');
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider>
+      <GlobalStyle />
       <App />
     </Provider>
   </React.StrictMode>,
-  rootElement,
+  rootElement
 );
