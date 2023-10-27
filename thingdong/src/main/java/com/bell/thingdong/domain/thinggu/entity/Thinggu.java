46b81208a39c905e539ffa17935d283fbc62cdf3
윤민재 <w124564@naver.com>
2023-10-27 13:23:28 +0900

    [S09P31B304-183] feat: 친구 요청 api 추가

--- thingdong/src/main/java/com/bell/thingdong/domain/thinggu/entity/Thinggu.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/thinggu/entity/Thinggu.java
@@ -1,13 +1,13 @@
 package com.bell.thingdong.domain.thinggu.entity;
 
+import com.bell.thingdong.domain.thinggu.dto.ThingguId;
 import com.bell.thingdong.domain.user.entity.User;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.IdClass;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
@@ -22,15 +22,17 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
+@IdClass(ThingguId.class)
 @Table(name = "thinggus")
 public class Thinggu {
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "thinggu_id", nullable = false)
-	private Long thingguId;
-
 	@OneToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "user_id", nullable = false)
+	@JoinColumn(name = "thinggu_id")
+	private User thingguId;
+
+	@Id
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
 	private User userId;
 
 	@Column(name = "thinggu_status", nullable = false)
