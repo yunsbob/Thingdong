1403fad084eb8a978f9e611ff155af85b847e853
윤민재 <w124564@naver.com>
2023-10-29 15:38:26 +0900

    [S09P31B304-183] fix: 띵구 삭제 에러 해결

--- thingdong/src/main/java/com/bell/thingdong/domain/thinggu/repository/ThingguRepository.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/thinggu/repository/ThingguRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.bell.thingdong.domain.thinggu.entity.Thinggu;
 
 public interface ThingguRepository extends JpaRepository<Thinggu, Long>, CustomThingguRepository {
+	public void deleteByThingguId(Long thingguId);
 }
