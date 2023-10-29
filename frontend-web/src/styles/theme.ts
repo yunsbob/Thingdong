1d698cef635150ead9fdb47a4c9d13a87228e606
이채림 <leecr1215@naver.com>
2023-10-29 14:48:35 +0900

    [S09P31B304-237] feat : 글로벌 스타일 자동완성 되도록 type 수정

--- frontend-web/src/styles/theme.ts
+++ frontend-web/src/styles/theme.ts
@@ -21,28 +21,30 @@ const color = {
 } as const;
 
 const fontSize = {
-    heading1: '46px',
-    heading2: '36px',
-    subtitle1: '36px',
-    subtitle2: '24px',
-    body1: '20px',
-    body2: '18px ',
-    body3: '16px',
-    body4: '14px',
-    small1: '12px',
-    small2: '10px',
+  heading1: '46px',
+  heading2: '36px',
+  subtitle1: '36px',
+  subtitle2: '24px',
+  body1: '20px',
+  body2: '18px ',
+  body3: '16px',
+  body4: '14px',
+  small1: '12px',
+  small2: '10px',
 } as const;
 
 const shadow = {
-    shadow: "0px 10px 25px rgba(190, 190, 190, 0.35)",
-    shadowBtn: "1px 1px 10px rgba(0, 0, 0, 0.10)",
-    shadowWhite: "0px 0px 10px #FFF",
+  shadow: '0px 10px 25px rgba(190, 190, 190, 0.35)',
+  shadowBtn: '1px 1px 10px rgba(0, 0, 0, 0.10)',
+  shadowWhite: '0px 0px 10px #FFF',
 } as const;
 
-const theme: DefaultTheme = {
-    color,
-    fontSize,
-    shadow,
-}
+const theme = {
+  color,
+  fontSize,
+  shadow,
+};
+
+export type ThemeType = typeof theme;
 
 export default theme;
