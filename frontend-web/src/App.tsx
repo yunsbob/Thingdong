4f14aafa00319ff7a08224df9a13629fabb9a40c
hellobaemin <hellobaemin@gmail.com>
2023-10-30 21:42:43 +0900

    [S09P31B304-250] feat: 랜딩페이지 개발 완료

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -1,17 +1,15 @@
-import MainPage from '@/pages/Main/MainPage';
 import React from 'react';
-import SplineScene from '@/pages/Landing/LandingPage';
+import MainPage from '@/pages/Main/MainPage';
+import LandingPage from '@/pages/Landing/LandingPage';
+import { useState } from 'react';
 
-const App = () => (
-  <>
-    {/* 로그인 안 되어 있다면 로그인 페이지  */}
-
-    {/* 로그인 되어 있다면 메인 페이지  */}
-
-    {/* 임시로 스플라인 띄워보기 */}
-    {/* <SplineScene /> */}
-    <MainPage />
-  </>
-);
+const App = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  return (
+    <>
+      {isLogin ? <MainPage /> : <LandingPage />}
+    </>
+  );
+};
 
 export default App;
