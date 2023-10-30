c7878b5594bc65d7b1a8e300e887d16500ffb54d
윤민재 <w124564@naver.com>
2023-10-30 21:18:10 +0900

    [S09P31B304-296] feat: 방명록 작성 api 개발

--- thingdong/src/main/java/com/bell/thingdong/domain/guestbook/service/GuestBookService.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/guestbook/service/GuestBookService.java
@@ -3,6 +3,11 @@ package com.bell.thingdong.domain.guestbook.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.bell.thingdong.domain.guestbook.dto.request.GuestBookReq;
+import com.bell.thingdong.domain.guestbook.entity.GuestBook;
+import com.bell.thingdong.domain.guestbook.repository.GuestBookRepository;
+import com.bell.thingdong.domain.user.repository.UserRepository;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -11,4 +16,15 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class GuestBookService {
+	private final UserRepository userRepository;
+	private final GuestBookRepository guestBookRepository;
+
+	@Transactional
+	public void createGuestBook(String email, GuestBookReq guestBookReq) {
+		GuestBook guestBook = GuestBook.builder().userEmail(guestBookReq.getUserId()).writerEmail(email).content(guestBookReq.getContent()).build();
+
+		System.out.println(guestBookReq.getContent());
+
+		guestBookRepository.save(guestBook);
+	}
 }
