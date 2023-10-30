39a48c009c8fba9505eb30f6620dc83313506fa6
이채림 <leecr1215@naver.com>
2023-10-30 13:56:34 +0900

    [S09P31B304-235]feat : 띵구 페이지 알림 개수 설정 및 mock data 추가

--- frontend-web/src/components/organisms/FriendList/FriendList.tsx
+++ frontend-web/src/components/organisms/FriendList/FriendList.tsx
@@ -2,55 +2,14 @@ import FriendBlock from '@/components/molecules/FriendBlock/FriendBlock';
 import * as S from '@/components/organisms/FriendList/FriendList.style';
 import { User } from '@/interfaces/User';
 
-const FriendList = () => {
-  let mockData: Array<User>;
+interface FriendListProps {
+  friends: User[];
+}
 
-  mockData = [
-    {
-      userId: 'hellomin1',
-      nickname: '잘생긴 토마토',
-    },
-    {
-      userId: 'hellomin2',
-      nickname: '잘생긴감자',
-    },
-    {
-      userId: 'aaaaaaaaaaaa',
-      nickname: '잘생긴 고구미',
-    },
-    {
-      userId: 'minmin123',
-      nickname: '멋쟁이 토마토',
-    },
-    {
-      userId: 'hellobaemin',
-      nickname: '못생긴 감자',
-    },
-    {
-      userId: 'jaehyun',
-      nickname: '못생긴 고구마',
-    },
-    {
-      userId: 'chelim',
-      nickname: '못생긴 고구마',
-    },
-    {
-      userId: 'minjae',
-      nickname: '못생긴 고구마',
-    },
-    {
-      userId: 'junho',
-      nickname: '못생긴 고구마',
-    },
-    {
-      userId: 'minseo',
-      nickname: '못생긴 고구마',
-    },
-  ];
-
+const FriendList = ({ friends }: FriendListProps) => {
   return (
     <S.FriendListContainer>
-      {mockData.map(user => {
+      {friends.map((user: User) => {
         return (
           <FriendBlock
             key={user.userId}
--- frontend-web/src/pages/Friend/FriendPage.tsx
+++ frontend-web/src/pages/Friend/FriendPage.tsx
@@ -6,18 +6,69 @@ import { Text } from '@/components/atoms/Text/Text.styles';
 import notification from '@/assets/images/friend/notification.png';
 import search from '@/assets/images/friend/search.png';
 import FriendList from '@/components/organisms/FriendList/FriendList';
+import { User } from '@/interfaces/User';
 
 const FriendPage = () => {
+  let mockData: Array<User>;
+
+  mockData = [
+    {
+      userId: 'hellomin1',
+      nickname: '잘생긴 토마토',
+    },
+    {
+      userId: 'hellomin2',
+      nickname: '잘생긴감자',
+    },
+    {
+      userId: 'aaaaaaaaaaaa',
+      nickname: '잘생긴 고구미',
+    },
+    {
+      userId: 'minmin123',
+      nickname: '멋쟁이 토마토',
+    },
+    {
+      userId: 'hellobaemin',
+      nickname: '못생긴 감자',
+    },
+    {
+      userId: 'jaehyun',
+      nickname: '못생긴 고구마',
+    },
+    {
+      userId: 'chelim',
+      nickname: '못생긴 고구마',
+    },
+    {
+      userId: 'minjae',
+      nickname: '못생긴 고구마',
+    },
+    {
+      userId: 'junho',
+      nickname: '못생긴 고구마',
+    },
+    {
+      userId: 'minseo',
+      nickname: '못생긴 고구마',
+    },
+  ];
+
   return (
     <S.FriendContainer>
       <Header text="띵구">
         <S.FriendHeaderIcons>
-          <Image src={notification} width={100} height={100} $unit="%" />
           <Image src={search} width={100} height={100} $unit="%" />
+          <Image src={notification} width={100} height={100} $unit="%" />
         </S.FriendHeaderIcons>
       </Header>
+      <S.NotificationNumberIcon>
+        <Text size="small2" fontWeight="heavy" color="white">
+          {mockData.length}
+        </Text>
+      </S.NotificationNumberIcon>
       <S.Sun />
-      <FriendList />
+      <FriendList friends={mockData} />
     </S.FriendContainer>
   );
 };
