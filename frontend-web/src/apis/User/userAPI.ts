1be1d555e3c389e5b08fed61e35b6865c03b4067
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-10-31 13:16:49 +0900

    [S09P31B304-353] feat: userAPI 생성

--- frontend-web/src/apis/User/userAPI.ts
+++ frontend-web/src/apis/User/userAPI.ts
@@ -1,9 +1,8 @@
-import { instance } from "@/apis/instance";
-import { UserInfo } from "@/types/user";
+import { instance } from '@/apis/instance';
+import { UserInfo } from 'types/user';
 
 const addUser = async (data: UserInfo) => {
-    const response = await instance.post("/users/join", data);
-    // console.log(response.data, "addUser안에서 localstorage에 userId 저장");
-    localStorage.setItem("userId", response.data.userId);
-    console.log("userId", response.data.userId, '추가');
-  };
+  await instance.post('/users/signUp', data);
+};
+
+export { addUser };
