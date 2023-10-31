59dca7eabe399ebd2f83f07fe3287b6db222c6ef
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-10-31 20:39:31 +0900

    [S09P31B304-208] fix : 띵스토리 key 에러 수정

--- frontend-web/src/components/molecules/ThingStoryItem/ThingStoryItem.styles.ts
+++ frontend-web/src/components/molecules/ThingStoryItem/ThingStoryItem.styles.ts
@@ -7,7 +7,7 @@ const ThingContainer = styled.div`
   width: 100%;
   height: fit-content;
   overflow-y: auto;
-  padding: 18px;
+  padding: 20px;
   border-radius: 22px;
   margin-top: 20px;
   background-color: ${({ theme }) => theme.color.white};
@@ -41,4 +41,5 @@ const Hr = styled.div`
     display: none;
   }
 `;
+
 export { ThingContainer, ThingWrapper, ThingLeftWrapper, TextWrapper, Hr };
