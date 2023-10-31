8b8a9e4ae5aa70f4181281aee24c531f9c3a5f72
윤민재 <w124564@naver.com>
2023-10-31 15:07:04 +0900

    [S09P31B304-294] feat: 언박띵 내역 관련 repository 설계 및 entity column 추가

--- thingdong/src/main/java/com/bell/thingdong/domain/object/entity/UnBoxThingHistory.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/object/entity/UnBoxThingHistory.java
@@ -9,6 +9,8 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -28,6 +30,10 @@ import lombok.NoArgsConstructor;
 @Table(name = "unboxthing_history")
 public class UnBoxThingHistory {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "objcet_history_id", nullable = false)
+	private Long objectHistoryId;
+
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "object_id", nullable = false)
 	private Object object;
