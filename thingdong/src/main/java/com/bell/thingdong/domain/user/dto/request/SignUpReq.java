fdc57e41a2c3adc169fd6030130c319fb72d200b
문준호 <tjsduq0423@naver.com>
2023-10-31 17:56:59 +0900

    [S09P31B304-000] 회원가입 제약조건 추가

--- thingdong/src/main/java/com/bell/thingdong/domain/user/dto/request/SignUpReq.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/user/dto/request/SignUpReq.java
@@ -3,7 +3,6 @@ package com.bell.thingdong.domain.user.dto.request;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +14,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class SignUpReq {
 	@NotEmpty(message = "ID는 필수 입력값입니다.")
-	@Pattern(regexp = "^[A-Za-z0-9._%+-]+$", message = "ID 형식에 맞지 않습니다.")
+	@Pattern(regexp = "^[A-Za-z0-9._%+-]{4,16}$", message = "ID 형식에 맞지 않습니다.")
 	@Schema(description = "userID", example = "ssafy")
-	@Size(min = 4, max = 16, message = "ID는 4자에서 16자 사이여야 합니다.")
 	private String userId;
 
 	@NotEmpty(message = "비밀번호는 필수 입력값입니다.")
@@ -26,6 +24,6 @@ public class SignUpReq {
 	@Schema(description = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.", example = "1q2w3e4r!")
 	private String password;
 
-	@Schema(description = "사용자 닉네임을 입력주세요 입력하지 않으면 자동으로 만들어 줍니다.",example = "돌아온 옥수수")
+	@Schema(description = "사용자 닉네임을 입력주세요 입력하지 않으면 자동으로 만들어 줍니다.", example = "돌아온 옥수수")
 	private String nickname;
 }
