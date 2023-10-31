ec197b08d8301d65cda8fd1fc5348176e26f5e28
jaehyeon <122436486+dubu777@users.noreply.github.com>
2023-10-31 20:21:44 +0900

    [S09P31B304-208] feat : 띵스토리 페이지 화면 ui 개발

--- frontend-web/src/components/molecules/ThingStoryItem/ThingStoryItem.styles.ts
+++ frontend-web/src/components/molecules/ThingStoryItem/ThingStoryItem.styles.ts
@@ -2,14 +2,43 @@ import styled from 'styled-components';
 
 const ThingContainer = styled.div`
   display: flex;
+  flex-direction: column;
   align-items: center;
-  justify-content: center;
   width: 100%;
-  height: 100%;
+  height: fit-content;
+  overflow-y: auto;
+  padding: 18px;
   border-radius: 22px;
   margin-top: 20px;
   background-color: ${({ theme }) => theme.color.white};
   box-shadow: ${({ theme }) => theme.shadow.shadow};
 `;
-
-export { ThingContainer };
+const ThingWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  gap: 28px;
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
+  }
+`;
+const ThingLeftWrapper = styled.div`
+  display: flex;
+`;
+const TextWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 10px;
+  justify-content: center;
+`;
+const Hr = styled.div`
+  width: 100%;
+  border-bottom: 1px solid ${({ theme }) => theme.color.grey4};
+  margin-bottom: 20px;
+  &:last-child {
+    display: none;
+  }
+`;
+export { ThingContainer, ThingWrapper, ThingLeftWrapper, TextWrapper, Hr };
--- frontend-web/src/components/molecules/ThingStoryItem/ThingStoryItem.tsx
+++ frontend-web/src/components/molecules/ThingStoryItem/ThingStoryItem.tsx
@@ -2,23 +2,70 @@ import { Image } from '@/components/atoms/Image/Image';
 import { Text, TextProps } from '@/components/atoms/Text/Text.styles';
 import * as S from './ThingStoryItem.styles';
 
+const historyList = [
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 10 },
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 20 },
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 110 },
+  { isPlus: false, name: '아이템 구매', date: '23.10.31', cost: 10 },
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 8 },
+  { isPlus: false, name: '아이템 구매', date: '23.10.31', cost: 10 },
+  { isPlus: false, name: '아이템 구매', date: '23.10.31', cost: 50 },
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 10 },
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 15 },
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 15 },
+  { isPlus: false, name: '아이템 구매', date: '23.10.31', cost: 10 },
+  { isPlus: false, name: '아이템 구매', date: '23.10.31', cost: 50 },
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 15 },
+  { isPlus: false, name: '아이템 구매', date: '23.10.31', cost: 10 },
+  { isPlus: false, name: '아이템 구매', date: '23.10.31', cost: 50 },
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 15 },
+  { isPlus: true, name: '띵구네 방문', date: '23.10.31', cost: 15 },
+];
+
 const ThingStoryItem = () => {
   return (
     <S.ThingContainer>
-      {/* <Text
-        size="small1"
-        fontWeight="extraBold"
-        color="grey1"
-        $marginLeft="2px"
-      >
-        불가
-      </Text> */}
-      <Image
-        src={require('@/assets/images/Thing/thing-minus.png').default}
-        $unit={'px'}
-        width={31}
-        height={31}
-      />
+      {historyList.map((item, index) => (
+        <>
+        <S.ThingWrapper key={index}>
+          <S.ThingLeftWrapper>
+            {item.isPlus ? (
+              <Image
+                src={require('@/assets/images/Thing/thing-plus.png').default}
+                $unit={'px'}
+                width={50}
+                height={50}
+              />
+            ) : (
+              <Image
+                src={require('@/assets/images/Thing/thing-minus.png').default}
+                $unit={'px'}
+                width={50}
+                height={50}
+              />
+            )}
+            <S.TextWrapper>
+              <Text size="body3" fontWeight="bold">
+                {item.name}
+              </Text>
+              <Text size="small2" fontWeight="bold" color="grey2" $marginTop='3px'>
+                {item.date}
+              </Text>
+            </S.TextWrapper>
+          </S.ThingLeftWrapper>
+          {item.isPlus ? (
+            <Text size="body3" fontWeight="bold">
+              + {item.cost}띵
+            </Text>
+          ) : (
+            <Text size="body3" fontWeight="bold">
+              - {item.cost}띵
+            </Text>
+          )}
+        </S.ThingWrapper>
+        <S.Hr/>
+        </>
+      ))}
     </S.ThingContainer>
   );
 };
