9439caad2999cb2c1b5274e27df15e0fda85d418
윤민재 <w124564@naver.com>
2023-10-31 14:46:02 +0900

    [S09P31B304-294] remove: 필요 없는 데이터 삭제

--- thingdong/src/main/java/com/bell/thingdong/domain/object/dto/UserObjectDto.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/object/dto/UserObjectDto.java
@@ -14,9 +14,6 @@ public class UserObjectDto {
 	@Schema(description = "유저가 가진 오브젝트 id", example = "2")
 	private Long userObjectId;
 
-	@Schema(description = "오브젝트 id", example = "2")
-	private Long objectId;
-
 	@Schema(description = "오브젝트 파일 경로", example = "wqexqwax")
 	private String objectPath;
 }
