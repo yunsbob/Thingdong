929b2368b7c98ab7759dcfe8658879611a63cecd
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-10-31 14:54:36 +0900

    [S09P31B304-358] feat: Slick 개발 중

--- frontend-web/src/pages/Splash/SplashPage.tsx
+++ frontend-web/src/pages/Splash/SplashPage.tsx
@@ -1,7 +1,9 @@
 import { Background } from '@/components/atoms/Background/Background.style';
+import Button from '@/components/atoms/Button/Button';
+import Slider from "react-slick";
 
 const SplashPage = () => {
-  return (<Background>123</Background>);
+  return (<Background><Button>나만의 방 꾸미기</Button>123</Background>);
 };
 
 export default SplashPage;
