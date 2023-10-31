5e9d7340ad63c7384f4f17369bbf4ebe94b45889
이채림 <leecr1215@naver.com>
2023-10-31 16:13:41 +0900

    [S09P31B304-235] feat : input debounce 적용

--- frontend-web/src/pages/Friend/Search/FriendSearchPage.tsx
+++ frontend-web/src/pages/Friend/Search/FriendSearchPage.tsx
@@ -6,17 +6,37 @@ import searchBtn from '@/assets/images/friend/search/searchBtn.png';
 import { Image } from '@/components/atoms/Image/Image';
 import Input from '@/components/atoms/Input/Input';
 import theme from '@/styles/theme';
+import { User } from '@/interfaces/user';
+import { useEffect, useState } from 'react';
+import { useDebounce } from '@/hooks/useDebounce';
 
 const FriendSearchPage = () => {
+  const [searchName, setSearchName] = useState('');
+
+  const onChangeName = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchName(e.target.value);
+    if (!!e.target.value) {
+      //TODO: api 호출
+    }
+  };
+
+  const debounceOnChange = useDebounce(onChangeName, 500);
+
   return (
     <Background $backgroundColor={theme.color.white}>
       <S.FriendSearchContainer>
         <Header text="띵구 찾기" hasBackButton={true} />
         <S.FriendSearchInputContainer>
-          <S.FriendSearchInput $inputSize="small" option="grey" />
+          <S.FriendSearchInput
+            $inputSize="small"
+            option="grey"
+            onChange={debounceOnChange}
+            value={searchName}
+          />
           <S.FriendSearchButton src={searchBtn} width={1.5} height={1.5} />
         </S.FriendSearchInputContainer>
       </S.FriendSearchContainer>
+      <div></div>
     </Background>
   );
 };
