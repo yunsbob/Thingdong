32068490d87648bc13f167f477cf29ad032f8ad7
윤민재 <w124564@naver.com>
2023-10-31 14:46:56 +0900

    [S09P31B304-294] feat: 언박띵 내역 테이블 추가

---
+++ thingdong/src/main/java/com/bell/thingdong/domain/object/entity/UnBoxThingHistory.java
@@ -0,0 +1,44 @@
+package com.bell.thingdong.domain.object.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+@Table(name = "unboxthing_history")
+public class UnBoxThingHistory {
+	@Id
+	@OneToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "object_id", nullable = false)
+	private Object object;
+
+	@Column(name = "user_email", nullable = false)
+	private String userEmail;
+
+	@Column(name = "object_name", nullable = false)
+	private String objectName;
+
+	@CreatedDate
+	@Column(name = "purchase_day", updatable = false)
+	private LocalDateTime purchaseDay;
+}
