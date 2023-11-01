663b9eb630f0ca3491e20d55ac5151cdde467938
윤민재 <w124564@naver.com>
2023-11-01 11:27:56 +0900

    [S09P31B304-294] feat: Object 관련 Dto 설계

--- thingdong/src/main/java/com/bell/thingdong/domain/object/dto/ObjectInventoryDto.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/object/dto/ObjectInventoryDto.java
@@ -10,15 +10,12 @@ import lombok.NoArgsConstructor;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class UserObjectInventoryDto {
+public class ObjectInventoryDto {
 	@Schema(description = "유저가 가진 오브젝트 id", example = "2")
 	private Long userObjectId;
 
 	@Schema(description = "오브젝트 파일 경로", example = "wqexqwax")
 	private String objectPath;
-
-	@Schema(description = "유저가 가진 오브젝트의 room id", example = "2")
-	private Long roomId;
 
 	@Schema(description = "오브젝트의 가격, 0이면 언박띵", example = "20")
 	private Long objectThing;
