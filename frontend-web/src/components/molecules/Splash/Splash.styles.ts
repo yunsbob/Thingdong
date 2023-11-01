dd76f88aac16ddfd76e2a4c2cae09ad60940f10d
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-11-01 14:46:40 +0900

    [S09P31B304-358] feat: splash2 완성, Text 크기 body3에서 2로 변경

--- frontend-web/src/components/molecules/Splash/Splash.styles.ts
+++ frontend-web/src/components/molecules/Splash/Splash.styles.ts
@@ -4,12 +4,12 @@ export const SlideButtonWrapper = styled.section`
   padding: 3.75rem 3.75rem 0 3.75rem;
 `;
 
-export const SlideTextWrapper = styled.section`
-  margin-top: 22px;
-  margin-bottom: 64px;
-  text-align: center;
-`;
+export interface SlideTextWrapperProps {
+  $marginBottom?: string;
+}
 
-export const TempContainer = styled.div`
-  /* border: 1px solid red; */
+export const SlideTextWrapper = styled.section<SlideTextWrapperProps>`
+  margin-top: 22px;
+  margin-bottom: ${props => props.$marginBottom || '64px'};
+  text-align: center;
 `;
--- frontend-web/src/components/molecules/Splash/Splash.tsx
+++ frontend-web/src/components/molecules/Splash/Splash.tsx
@@ -1,49 +1,47 @@
 import Button from '@/components/atoms/Button/Button';
-import theme from '@/styles/theme';
-import {
-  SlideButtonWrapper,
-  SlideTextWrapper,
-  TempContainer,
-} from './Splash.styles';
+import { SlideButtonWrapper, SlideTextWrapper } from './Splash.styles';
 import { Text } from '@/components/atoms/Text/Text.styles';
 import { Image } from '@/components/atoms/Image/Image';
 import room from '@/assets/images/splash/splash1_room.png';
+import write from '@/assets/images/splash/splash2_write.png';
 
 export const Splash1 = () => {
   return (
-    <TempContainer>
+    <>
       <SlideButtonWrapper>
         <Button $backgroundColor="white" $color="black1">
           나만의 방 꾸미기
         </Button>
       </SlideButtonWrapper>
       <SlideTextWrapper>
-        <Text size="body3" $lineHeight="1.5" fontWeight="bold">
+        <Text size="body2" $lineHeight="1.5" fontWeight="bold">
           내가 원하는 가구, 소품을 배치해
           <br />
           나만의 방을 꾸밀 수 있어요
         </Text>
       </SlideTextWrapper>
-
       <Image src={room} width={22} />
-    </TempContainer>
+    </>
   );
 };
 
 export const Splash2 = () => {
   return (
-    <SlideButtonWrapper>
-      <Button $backgroundColor="white" $color="black1">
-        띵구 맺기
-      </Button>
-      <SlideTextWrapper>
-        <Text size="body3" $lineHeight="1.5" fontWeight="bold">
-          띵구 맺고 띵구집을 방문해보세요!
-          <br />
-          방명록도 남길 수 있습니다 🥰
-        </Text>
-      </SlideTextWrapper>
-    </SlideButtonWrapper>
+    <>
+      <SlideButtonWrapper>
+        <Button $backgroundColor="white" $color="black1">
+          띵구 맺기
+        </Button>
+        <SlideTextWrapper $marginBottom="30px">
+          <Text size="body2" $lineHeight="1.5" fontWeight="bold">
+            내 친구 띵구네 집을 방문해보세요!
+            <br />
+            방명록도 남길 수 있습니다 🥰
+          </Text>
+        </SlideTextWrapper>
+      </SlideButtonWrapper>
+      <Image src={write} />
+    </>
   );
 };
 
@@ -56,12 +54,12 @@ export const Splash3 = () => {
         </Button>
       </SlideButtonWrapper>
       <SlideTextWrapper>
-        <Text size="body3" $lineHeight="1.5" fontWeight="bold">
+        <Text size="body2" $lineHeight="1.5" fontWeight="bold">
           스마트싱스 앱에 등록한 기기를
           <br />
           띵동으로 가져올 수 있어요.
           <br />
-          가져온 띵즈를 나만의 방에 배치해보세요 !
+          나만의 방에 띵즈를 배치해보세요 !
         </Text>
       </SlideTextWrapper>
     </>
@@ -77,7 +75,7 @@ export const Splash4 = () => {
         </Button>
       </SlideButtonWrapper>
       <SlideTextWrapper>
-        <Text size="body3" $lineHeight="1.5" fontWeight="bold">
+        <Text size="body2" $lineHeight="1.5" fontWeight="bold">
           인벤토리를 확인하면 <br />
           내가 가진 아이템을 목록을 확인할 수 있어요
         </Text>
@@ -95,7 +93,7 @@ export const Splash5 = () => {
         </Button>
       </SlideButtonWrapper>
       <SlideTextWrapper>
-        <Text size="body3" $lineHeight="1.5" fontWeight="bold">
+        <Text size="body2" $lineHeight="1.5" fontWeight="bold">
           추억을 담은 나만의 물건을 언박띵하세요! <br />
           띵구에게 선물도 가능하답니다
         </Text>
