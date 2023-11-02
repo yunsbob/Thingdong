54cd4245edbf8b055df317d2904156f44640cbbb
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-11-02 17:56:15 +0900

    [S09P31B304-365] AppState 관련 라이브러리 찾는 중

--- frontendapp/App.tsx
+++ frontendapp/App.tsx
@@ -6,16 +6,30 @@
  */
 
 import React from 'react';
-// import {SafeAreaView, Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
 import WebView from 'react-native-webview';
+// import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function App(): JSX.Element {
+  // useEffect(() => {
+  //   // Component가 마운트될 때 로컬 스토리지를 클리어.
+  //   // Splash 확인용으로 자동 로그인 방지하기 위함
+  //   clearLocalStorage();
+  // }, []);
+
+  // const clearLocalStorage = async () => {
+  //   try {
+  //     await AsyncStorage.clear();
+  //   } catch (error) {
+  //     console.error('Error clearing local storage:', error);
+  //   }
+  // };
+
   return (
-    // <SafeAreaView>
-    // <WebView source={{uri: 'https://thingdong.com'}} />
-    <WebView source={{uri: 'http://192.168.31.240:3000/'}} />
-    // <Text> 123</Text>
-    // </SafeAreaView>
+    <SafeAreaView style={{flex: 1}}>
+      <WebView source={{uri: 'https://thingdong.com'}} />
+      {/* <WebView source={{uri: 'http://192.168.31.240:3000/'}} /> */}
+    </SafeAreaView>
   );
 }
 
