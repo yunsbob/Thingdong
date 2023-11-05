394bb066019e6871127828a692190f225c0cc89c
hellobaemin <hellobaemin@gmail.com>
2023-11-05 20:52:50 +0900

    [S09P31B304-363] feat: 방명록 이미지 화면에 렌더링

--- frontend-web/src/pages/FriendRoom/FriendRoomPage.styles.ts
+++ frontend-web/src/pages/FriendRoom/FriendRoomPage.styles.ts
@@ -1,6 +1,8 @@
 import styled from 'styled-components';
 import { RoomName } from '@/pages/Home/Home.styles';
 import { Image } from '@/components/atoms/Image/Image';
+import Modal from '@/components/molecules/Modal/Modal';
+import { redFace } from '@/assets/images/friend/face-red.png';
 
 export const FriendRoomName = styled(RoomName)`
   margin: 0;
@@ -33,4 +35,37 @@ export const BackButton = styled(Image)`
   top: 4%;
   left: -20%;
   transform: scale(60%);
+`;
+
+export const GuestbookModal = styled(Modal)`
+  display: flex;
+  flex-direction: column;
+  background-color: #00000000;
+  transform: translateY(-10%);
+  img {
+    align-self: center;
+  }
+`;
+
+export const WriteArea = styled.div`
+  position: absolute;
+  top: 31%;
+  background-color: blue;
+  height: 100%;
+  max-height: 12.5rem;
+  width: 100%;
+  max-width: 79%;
+  text-align: start;
+  padding: 3px;
+`;
+
+export const WriterArea = styled.div`
+  /* position: absolute; */
+  /* top: 31%; */
+  background-color: red;
+  height: 100%;
+  max-height: 2.5rem;
+  width: 100%;
+  text-align: start;
+  /* padding: 3px; */
 `;
--- frontend-web/src/pages/FriendRoom/FriendRoomPage.tsx
+++ frontend-web/src/pages/FriendRoom/FriendRoomPage.tsx
@@ -3,15 +3,43 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import * as S from './FriendRoomPage.styles';
 import backButtonWhite from '@/assets/images/friend/search/back-white.png';
 import Button from '@/components/atoms/Button/Button';
+import { changeModalOpen } from '@/utils/changeModalOpen';
+import { useState } from 'react';
+import { Image } from '@/components/atoms/Image/Image';
+import guestbook from '@/assets/images/room/guestbook.png';
+import { Text } from '@/components/atoms/Text/Text.styles';
 
 const FriendRoomPage = () => {
   const location = useLocation();
   const { userId, nickname } = location.state || {};
   const navigate = useNavigate();
+  const [modalOpen, setModalOpen] = useState(true);
 
   return (
     <>
       <S.FriendRoomHeader>
+        <S.GuestbookModal
+          isOpen={modalOpen}
+          onClose={() => changeModalOpen(modalOpen, setModalOpen)}
+          height="auto"
+          $unit=""
+        >
+          <Image src={guestbook} width={21} />
+          <S.WriteArea>
+            <Text size="body2" fontWeight="bold" $lineHeight="1.5">
+              이 편지는 영국에서 최초로 시작되어 일년에 한 바퀴를 돌면서 받는
+              사람에게 행운을 주었습니당
+            </Text>
+            <S.WriterArea>
+          <Text size="body2" fontWeight="bold" color="grey1">
+            작성자 
+            </Text>
+          </S.WriterArea>
+          </S.WriteArea>
+          
+          <Button>작성하기</Button>
+        </S.GuestbookModal>
+
         <S.BackButton
           src={backButtonWhite}
           onClick={() => navigate(-1)}
@@ -21,9 +49,11 @@ const FriendRoomPage = () => {
       {/* TODO: 각 띵구 userId로 방 상태 DB로부터 불러와야함 */}
       {/* 임시 방명록용 버튼 띄우기 */}
       <S.TempGuestbookBtnWrapper>
-        <Button>방명록 작성</Button>
+        <Button onClick={() => changeModalOpen(modalOpen, setModalOpen)}>
+          방명록 작성
+        </Button>
       </S.TempGuestbookBtnWrapper>
-      <FriendRoomScene />
+      {/* <FriendRoomScene /> */}
     </>
   );
 };
