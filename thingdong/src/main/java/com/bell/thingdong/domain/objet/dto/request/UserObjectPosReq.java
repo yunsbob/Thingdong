d8ed2d4df12181dee8b74aeaa08f6d0d2bbb085c
윤민재 <w124564@naver.com>
2023-11-08 16:49:07 +0900

    [S09P31B304-485] feat : 오브젝트 관련 entity 설계 변경 및 오브젝트 설치 dto 설계

---
+++ thingdong/src/main/java/com/bell/thingdong/domain/objet/dto/request/UserObjectPosReq.java
@@ -0,0 +1,28 @@
+package com.bell.thingdong.domain.objet.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserObjectPosReq {
+	@Schema(description = "방 번호", example = "1")
+	private Long roomId;
+
+	@Schema(description = "오브젝트 x 좌표 값", example = "1")
+	private Double x;
+
+	@Schema(description = "오브젝트 y 좌표 값", example = "2")
+	private Double y;
+
+	@Schema(description = "오브젝트 z 좌표 값", example = "3")
+	private Double z;
+
+	@Schema(description = "오브젝트 회전 값", example = "2")
+	private Double rotation;
+}
