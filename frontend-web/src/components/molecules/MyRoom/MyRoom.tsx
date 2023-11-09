86f49f1c8226deb24a267b59d353dc0eb097c669
배민경 | Minkyoung Bae <hellobaemin@gmail.com>
2023-11-09 15:06:37 +0900

    [S09P31B304-469] feat: MyRoom tsx 파일로 변경

--- frontend-web/src/components/molecules/MyRoom/MyRoom.tsx
+++ frontend-web/src/components/molecules/MyRoom/MyRoom.tsx
@@ -3,8 +3,14 @@ import { Canvas } from '@react-three/fiber';
 import { OrbitControls } from '@react-three/drei';
 import { Spinner } from '../Spinner/Spinner';
 import MyGlbScene from '@/components/molecules/MyRoom/MyGlbScene';
+import { Position } from '@/types/room';
 
-const MyRoom = ({ isEditing, position }) => {
+interface MyRoomProps {
+  isEditing: boolean;
+  position: Position;
+}
+
+const MyRoom = ({ isEditing, position }: MyRoomProps) => {
   return (
     <div style={{ backgroundColor: 'skyblue', width: '100%', height: '100vh' }}>
       <Suspense fallback={<Spinner />}>
