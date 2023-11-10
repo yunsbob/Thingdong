433c3a2b201fc8342850c7662177ce821503e25b
윤민재 <w124564@naver.com>
2023-11-10 22:37:24 +0900

    [S09P31B304-512] feat : 설치된 오브제 좌표, 회전, 크기 값 추가

--- thingdong/src/main/java/com/bell/thingdong/domain/objet/dto/UserObjectRoomDto.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/objet/dto/UserObjectRoomDto.java
@@ -16,4 +16,19 @@ public class UserObjectRoomDto {
 
 	@Schema(description = "오브젝트 모델 파일 경로", example = "wqexqwax")
 	private String objectModelPath;
+
+	@Schema(description = "오브젝트 이름", example = "tomato")
+	private String name;
+
+	@Schema(description = "벽에 있는 오브젝트인지 표시", example = "false")
+	private Boolean isWall;
+
+	@Schema(description = "오브젝트 포지션")
+	private ObjectPositionDto position;
+
+	@Schema(description = "오브젝트 회전")
+	private ObjectRotationDto rotation;
+
+	@Schema(description = "오브젝트 크기")
+	private ObjectSizeDto size;
 }
