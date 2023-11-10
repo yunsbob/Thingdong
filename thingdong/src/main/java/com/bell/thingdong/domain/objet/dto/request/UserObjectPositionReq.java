4c04d5b00b31191faec8bb3dd61d922c488e3d6a
윤민재 <w124564@naver.com>
2023-11-10 22:56:29 +0900

    [S09P31B304-512] feat : 오브제 설치 관련 requestDto 변경

--- thingdong/src/main/java/com/bell/thingdong/domain/objet/dto/request/UserObjectPositionReq.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/objet/dto/request/UserObjectPositionReq.java
@@ -1,5 +1,8 @@
 package com.bell.thingdong.domain.objet.dto.request;
 
+import com.bell.thingdong.domain.objet.dto.ObjectPositionDto;
+import com.bell.thingdong.domain.objet.dto.ObjectRotationDto;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,15 +20,9 @@ public class UserObjectPositionReq {
 	@Schema(description = "오브제 번호", example = "1")
 	private Long userObjectId;
 
-	@Schema(description = "오브젝트 x 좌표 값", example = "1")
-	private Double x;
+	@Schema(description = "오브젝트 좌표 값")
+	private ObjectPositionDto position;
 
-	@Schema(description = "오브젝트 y 좌표 값", example = "2")
-	private Double y;
-
-	@Schema(description = "오브젝트 z 좌표 값", example = "3")
-	private Double z;
-
-	@Schema(description = "오브젝트 회전 값", example = "2")
-	private Double rotationY;
+	@Schema(description = "오브젝트 회전 값")
+	private ObjectRotationDto rotation;
 }
