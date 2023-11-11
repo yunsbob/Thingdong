b5c1e261f400cac456b9cc0599aee491f0108d49
윤민재 <w124564@naver.com>
2023-11-12 04:03:32 +0900

    [S09P31B304-512] feat : 스마트 띵스 상태 변경 api 추가

--- thingdong/src/main/java/com/bell/thingdong/domain/smartthings/entity/SmartThings.java
+++ thingdong/src/main/java/com/bell/thingdong/domain/smartthings/entity/SmartThings.java
@@ -5,6 +5,8 @@ import com.bell.thingdong.domain.objet.entity.UserObject;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
@@ -23,6 +25,10 @@ import lombok.NoArgsConstructor;
 @Table(name = "smart_things")
 public class SmartThings {
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "smart_things_id")
+	private Long smartThingsId;
+
 	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "user_object_id", nullable = false)
 	private UserObject userObject;
@@ -35,4 +41,8 @@ public class SmartThings {
 
 	@Column(name = "activation_path", nullable = false)
 	private String activationPath;
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
 }
